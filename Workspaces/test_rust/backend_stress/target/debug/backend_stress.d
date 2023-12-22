d7fb6d5f2dd5d5e18ccbaae9aa7a6ce0679c4e29
Jairfsj <Jairfsj@gmail.com>
2023-12-21 22:48:51 -0500

    clear

---
+++ Workspaces/test_rust/backend_stress/target/debug/backend_stress.d
@@ -0,0 +1,1 @@
+/home/jairfsj/Projects/Workspaces/test_rust/backend_stress/target/debug/backend_stress: /home/jairfsj/Projects/Workspaces/test_rust/backend_stress/src/main.rs
---
+++ Workspaces/test_rust/backend_stress/target/debug/deps/backend_stress-ede2bbafdf162dad.d
@@ -0,0 +1,5 @@
+/home/jairfsj/Projects/Workspaces/test_rust/backend_stress/target/debug/deps/backend_stress-ede2bbafdf162dad: src/main.rs
+
+/home/jairfsj/Projects/Workspaces/test_rust/backend_stress/target/debug/deps/backend_stress-ede2bbafdf162dad.d: src/main.rs
+
+src/main.rs:
