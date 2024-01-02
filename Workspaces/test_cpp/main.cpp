bb4547fa516a0a93a4998fb5045fcb8e5b7e0fdf
Jairfsj <jairfsj@gmail.com>
2024-01-02 16:58:15 -0500

    test

--- Workspaces/test_cpp/main.cpp
+++ Workspaces/test_cpp/main.cpp
@@ -1,6 +1,13 @@
+/*#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}*/ 
+
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World";
-  return 0;
+    std::cout << "Hello C++" << std::endl;
+    return 0;
 }
