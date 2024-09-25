8e05d8f53bc21fb9eb1dde21fee388c24a46c722
Jairfsj <jairfsj@gmail.com>
2024-09-25 18:22:29 -0400

    guessing game

--- Workspaces/test_rust/steps_rust/hello_world/target/debug/deps/hello_world-8488c151f292b869.d
+++
@@ -1,5 +0,0 @@
-/home/jairfsj/Projects/Workspaces/test_rust/steps_rust/hello_world/target/debug/deps/hello_world-8488c151f292b869: src/main.rs
-
-/home/jairfsj/Projects/Workspaces/test_rust/steps_rust/hello_world/target/debug/deps/hello_world-8488c151f292b869.d: src/main.rs
-
-src/main.rs:
