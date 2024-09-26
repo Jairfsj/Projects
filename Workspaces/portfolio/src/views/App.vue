7b953fd4b732236269327f987c5eb7d47e2e961e
Jairfsj <jairfsj@gmail.com>
2024-09-26 18:40:38 -0400

    Portifolio

---
+++ Workspaces/portfolio/src/views/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <header class="bg-blue-500 py-4">
+      <nav class="container mx-auto text-white text-center">
+        <a href="/" class="text-2xl font-bold">Portifolio de Jair Francisco da Silva</a>
+      </nav>
+    </header>
+
+    <main>
+      <skills />
+    </main>  
+    
+
+    <footer class="bg-gray-800 py-4 text-white text-center">
+      <p>© 2024 Jair francisco da Silva - Todos os direitos reservados</p>
+    </footer>  
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills.vue';
+
+export default {
+  components: {
+    Skills
+  }
+};
+</script>
+
+<style>
+/* Importar o Tailwind CSS */
+@import './assets/tailwind.css';
+</style>
---
+++ Workspaces/portfolio/src/views/main.js
@@ -0,0 +1,5 @@
+import { createApp} from 'vue';
+import App from './App.vue';
+
+//Criando a aplicacao Vue
+createApp(App).mount('#app');
