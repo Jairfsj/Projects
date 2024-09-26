9e98b1851a7d4f88036c7062c15203018d475ce7
Jairfsj <jairfsj@gmail.com>
2024-09-25 20:42:05 -0400

    Portifolio

---
+++ Workspaces/test_rust/steps_rust/projects/steps/test_rust/target/debug/deps/test_rust-a1b7ed9bdb85109d.d
@@ -0,0 +1,5 @@
+/home/jairfsj/Projects/Workspaces/test_rust/steps_rust/projects/steps/test_rust/target/debug/deps/libtest_rust-a1b7ed9bdb85109d.rmeta: src/main.rs
+
+/home/jairfsj/Projects/Workspaces/test_rust/steps_rust/projects/steps/test_rust/target/debug/deps/test_rust-a1b7ed9bdb85109d.d: src/main.rs
+
+src/main.rs:
---
+++ Workspaces/test_rust/steps_rust/projects/steps/test_rust/target/debug/deps/test_rust-e766d6f292c5a4a7.d
@@ -0,0 +1,5 @@
+/home/jairfsj/Projects/Workspaces/test_rust/steps_rust/projects/steps/test_rust/target/debug/deps/libtest_rust-e766d6f292c5a4a7.rmeta: src/main.rs
+
+/home/jairfsj/Projects/Workspaces/test_rust/steps_rust/projects/steps/test_rust/target/debug/deps/test_rust-e766d6f292c5a4a7.d: src/main.rs
+
+src/main.rs:
