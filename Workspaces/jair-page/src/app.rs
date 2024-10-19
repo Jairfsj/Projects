436ae9dd23b79ca7b75657490cd6874a558042a7
Jairfsj <jairfsj@gmail.com>
2024-10-19 16:23:54 -0400

    changes

--- Workspaces/jair-page/src/app.rs
+++ Workspaces/jair-page/src/app.rs
@@ -1,25 +1,12 @@
 use yew::prelude::*;
-use yew_router::prelude::*;
-
-mod components;
-use components::{landing_page, about_page, contact_page, navbar, Route};
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <BrowserRouter>
-            <Navbar />
-            <Switch<Route> render={Switch::render(switch)} />
-        <BrowserRouter>    
+        <main>
+            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+            <h1>{ "Hello World!" }</h1>
+            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+        </main>
     }
 }
-
-fn switch(routes: &Route) -> Html {
-    match routes {
-        Route::Home => html! { <landing_page.rs },
-        Route::About => html! { <about_page /> },
-        Route::Contact => html! { <contact_page /> },
-    }
-}
-
-    
