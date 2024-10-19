739b1753cd114ba57a3ee426f705c0eab1ec4a7e
Jairfsj <jairfsj@gmail.com>
2024-10-19 16:38:28 -0400

    changes

--- Workspaces/jair-page/src/app.rs
+++ Workspaces/jair-page/src/app.rs
@@ -5,8 +5,8 @@ pub fn app() -> Html {
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            <h1>{ "Bem Vindo!" }</h1>
+            <span class="subtitle">{ "Criando com Rust " }<i class="heart" /></span>
         </main>
     }
 }
